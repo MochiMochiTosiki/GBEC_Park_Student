bcaaae74e8d4443890f5673e409076c5c3ffd8ec
MochimochiToshiki <mochizuki@haw.co.jp>
2019-09-05 00:36:25 +0900

    create mint function

--- truffle/contracts/gbec_park_student.sol
+++ truffle/contracts/gbec_park_student.sol
@@ -3,5 +3,16 @@ pragma solidity^0.5.0;
 import "../node_modules/openzeppelin-solidity/contracts/token/ERC721/ERC721Full.sol";
 
 contract Student is ERC721Full {
+    string private _name = "GBEC's NFT";
+    string private _symbol = "GSFT";
+    uint256 private _tokenId = 0;
 
+    constructor()ERC721Full(_name, _symbol) public {}
+
+    function mint(address to, string memory pictHash) public {
+        require(to != msg.sender, "can only mint by snder");
+        _tokenId += 1;
+        _mint(to, _tokenId);
+        _setTokenURI(_tokenId, pictHash);
+    }
 }
